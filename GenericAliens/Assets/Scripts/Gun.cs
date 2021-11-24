48db65f274d3c59a985bfe369daba852aaac1a5c
Liamiguess <71291859+Liamiguess@users.noreply.github.com>
2021-11-23 21:21:14 -0500

    Added and changed gun script

---
+++ GenericAliens/Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    
+    public GameObject Flare;
+    public int GunRecoilTime;
+    private bool CoroutineIsNotRunning = true;
+
+    IEnumerator DisableFlare(float waitTime)
+    {
+    CoroutineIsNotRunning = false;
+    yield return new WaitForSeconds(waitTime);
+    Flare.SetActive(false);
+    CoroutineIsNotRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && CoroutineIsNotRunning == true)
+        {
+            Flare.SetActive(true);
+            StartCoroutine(DisableFlare(GunRecoilTime));
+
+        }
+
+    }
+}
