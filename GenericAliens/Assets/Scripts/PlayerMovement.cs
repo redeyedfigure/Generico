b61bb10ba2126abcf69f2dcd808b190569379c51
Redeyedfigure <34867686+redeyedfigure@users.noreply.github.com>
2021-11-24 08:38:05 +0700

    Tweaked the movement script, added running

--- GenericAliens/Assets/Scripts/PlayerMovement.cs
+++ GenericAliens/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] CharacterController controller;
 
     [Header("Basic Parameters")]
-    [SerializeField] float speed = 12f;
+    [SerializeField] float walkSpeed = 12f;
+    [SerializeField] float runSpeed = 18f;
+    [SerializeField] float smoothSpeed = 0.4f;
+
+    //Running-related material
+    public float currentSpeed;
+    bool isMoving;
 
     [Header("Ground Checking")]
     [SerializeField] float groundDistance = 0.4f;
@@ -32,9 +38,39 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
+        //Check if the player is moving
+        if(x != 0 || y != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        //Running and walking with linear interpolation
+        if(isMoving && !Input.GetKey(KeyCode.LeftShift) && currentSpeed != walkSpeed)
+        {
+            currentSpeed = Mathf.Lerp(currentSpeed, walkSpeed, smoothSpeed);
+        } 
+        else if(isMoving && Input.GetKey(KeyCode.LeftShift) && currentSpeed != runSpeed)
+        {
+            currentSpeed = Mathf.Lerp(currentSpeed, runSpeed, smoothSpeed);
+        }
+        else if(!isMoving)
+        {
+            currentSpeed = 0;
+        }
+
+        //Increasing performance(maybe) by autocorrecting numbers within a small range of the desired number
+        if (currentSpeed > (runSpeed - 0.05) && Input.GetKey(KeyCode.LeftShift))
+            currentSpeed = runSpeed;
+        if ((currentSpeed < (walkSpeed + 0.05) && currentSpeed > walkSpeed) || (currentSpeed > (walkSpeed - 0.05) && currentSpeed < walkSpeed) && !Input.GetKey(KeyCode.LeftShift))
+            currentSpeed = walkSpeed;
+
         //Basic Movement
         move = transform.right * x + transform.forward * y;
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * currentSpeed * Time.deltaTime);
 
         //Ground-Checking
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
